25419b548c338f576780dec2074681665f4c527b
RSADenchik <WorldRSA@gmail.com>
2016-12-16 05:54:57 +0200

    popups desktop

--- js/main.js
+++ js/main.js
@@ -41,4 +41,24 @@ $(document).ready(function() {
         myMap.geoObjects.add(myPlacemark);
 
     }
+
+    // moda dialog center
+    // $(function() {
+    //     function reposition() {
+    //         var modal = $(this),
+    //             dialog = modal.find('.modal-dialog');
+    //         modal.css('display', 'block');
+    //
+    //         // Dividing by two centers the modal exactly, but dividing by three
+    //         // or four works better for larger screens.
+    //         dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
+    //     }
+    //     // Reposition when a modal is shown
+    //     $('.modal').on('show.bs.modal', reposition);
+    //     // Reposition when the window is resized
+    //     $(window).on('resize', function() {
+    //         $('.modal:visible').each(reposition);
+    //     });
+    // });
+
 });
